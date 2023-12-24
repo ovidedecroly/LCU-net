97e9d92cd4c59ea7742d1eed62cf1d552d972c8c
Ovide Academy <ovide@staff.uns.ac.id>
2023-12-24 15:51:40 +0700

    update

--- lcu-net/LCU-C2f.py
+++ lcu-net/LCU-C2f.py
@@ -64,10 +64,20 @@ def LCU_Net_with_C2f(input_shape=(256, 256, 3), output_shape=(256, 256, 1), debu
     # Menggunakan __encoder_decoder_block dengan C2f
     block1, down_sampled1 = __encoder_decoder_block(input_tensor=input_tensor, c1=3, c2=16, encoder=True, dropout=0.0, maxpool=True, debug=debug)
     block2, down_sampled2 = __encoder_decoder_block(input_tensor=down_sampled1, c1=16, c2=32, encoder=True, dropout=0.0, maxpool=True, debug=debug)
-    # Lanjutkan dengan sisanya, menyesuaikan c1 dan c2 sesuai
 
-    # Output layer
-    output_block = Conv2D(filters=output_shape[-1], kernel_size=1, strides=1, padding='same', activation='sigmoid')(down_sampled2)
+    # Encoder Blocks with C2f
+    block3, down_sampled3 = __encoder_decoder_block(input_tensor=down_sampled2, c1=32, c2=64, encoder=True, dropout=0.0, maxpool=True, debug=debug)
+    block4, down_sampled4 = __encoder_decoder_block(input_tensor=down_sampled3, c1=64, c2=128, encoder=True, dropout=0.0, maxpool=True, debug=debug)
+    block5, down_sampled5 = __encoder_decoder_block(input_tensor=down_sampled4, c1=128, c2=256, encoder=True, dropout=0.0, maxpool=True, debug=debug)
+
+    # Decoder Blocks with C2f
+    block6, _ = __encoder_decoder_block(input_tensor=down_sampled5, c1=256, c2=128, encoder=False, concat_block=block4, debug=debug)
+    block7, _ = __encoder_decoder_block(input_tensor=block6, c1=128, c2=64, encoder=False, concat_block=block3, debug=debug)
+    block8, _ = __encoder_decoder_block(input_tensor=block7, c1=64, c2=32, encoder=False, concat_block=block2, debug=debug)
+    block9, _ = __encoder_decoder_block(input_tensor=block8, c1=32, c2=16, encoder=False, concat_block=block1, debug=debug)
+
+    # Final Convolutional Layer
+    output_block = Conv2D(filters=output_shape[-1], kernel_size=1, strides=1, padding='same', activation='sigmoid')(block9)
 
     lcu_net = tf.keras.Model(inputs=input_tensor, outputs=output_block)
     lcu_net.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -79,3 +89,4 @@ def LCU_Net_with_C2f(input_shape=(256, 256, 3), output_shape=(256, 256, 1), debu
 
 # Membuat model
 model = LCU_Net_with_C2f(debug=True)
+
